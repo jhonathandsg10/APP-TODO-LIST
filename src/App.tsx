a02530b4148492f04bf9f7d9fd20c48ba961888f
jhonathandsg10 <jhonathan.gomes@estudante.ifms.edu.br>
2023-05-03 22:09:26 -0400

    darkmode

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 function App() {
-
-
   return (
-    <>
-     <h1>TODO LIST</h1>
-    </>
-  )
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <main> TODO  LIST</main>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
