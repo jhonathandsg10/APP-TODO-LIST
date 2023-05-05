63ac12c54671e62b316d7d8990b7d23cab31f802
jhonathandsg10 <jhonathan.gomes@estudante.ifms.edu.br>
2023-05-04 21:07:54 -0400

    estruturando layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
-import { AppBar, Button, Grid, TextField, Toolbar, Typography } from '@mui/material/index';
+import { AppBar, Badge, Button, Container, Grid, TextField, Toolbar, Typography, colors } from '@mui/material/index';
 import { dark } from '@mui/material/styles/createPalette';
+import { CheckFat, PlusCircle, Rocket } from '@phosphor-icons/react';
 
 
 const darkTheme = createTheme({
@@ -16,34 +17,70 @@ function App() {
   return (
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
-      <AppBar position = "static">
+      <AppBar position="static">
         <Toolbar sx={{
-          paddingTop:theme.spacing(2),
-          paddingBottom:theme.spacing(2),
-          display:"flex",
-          justifyContent:'center',
-          alignItems:'center',
-          '@media all':{
-            minHeight:200,
+          paddingTop: theme.spacing(2),
+          paddingBottom: theme.spacing(2),
+          display: "flex",
+          justifyContent: 'center',
+          alignItems: 'center',
+          '@media all': {
+            minHeight: 200,
           }
 
         }}>
-            <Typography variant = "h5" component="h1">todo</Typography>
+          <Typography variant="h5" component="h1" sx={{
+            display: 'flex',
+            alignItems: 'center',
+            gap: theme.spacing(2),
+            color: theme.palette.primary.light
+          }}><CheckFat size={32} weight='fill' /><span> Lista de Tarefas</span>
+          </Typography>
         </Toolbar>
       </AppBar>
       <main>
-          <Grid container spacing={theme.spacing(0.5)}>
-              <Grid item xl={8}  sm={12}>
-                <TextField name='task' fullWidth>
+        <Container sx={{
+          position: 'relative',
 
-                </TextField>
+        }}>
+          <Grid container spacing={theme.spacing(0.5)} sx={{
+            position: 'absolute',
+            top: '-27px',
+          }}>
+            <Grid item xl={10} sm={12}>
+              <TextField placeholder='adicione uma nova tarefa' variant='outlined' name='task' fullWidth sx={{
+                backgroundColor: colors.grey[800]
+              }}>
+
+
+              </TextField>
+            </Grid>
+            <Grid item xl={2} sm={12}>
+              <Button variant='contained' sx={{
+                height: '100%'
+              }}><span>Criar</span><PlusCircle size={32} />
+
+              </Button>
+            </Grid>
+            <Grid container spacing={theme.spacing(1)} sx={{
+              marginTop:'25px'
+            }}>
+              <Grid item>
+                <Typography variant="caption"sx={{
+                  display:'flex',
+                  alignItems:"center",
+                  gap:theme.spacing(2)
+                }}>
+                  <span>Tarefas Criadas</span>
+                  <Badge color='primary' badgeContent={0} showZero></Badge>
+                </Typography>
+
               </Grid>
-              <Grid item xl={4} sm={12}>
-                <Button variant='contained'>Criar</Button>
-              </Grid>
+            </Grid>
           </Grid>
+        </Container>
       </main>
-      
+
     </ThemeProvider>
   );
 }
